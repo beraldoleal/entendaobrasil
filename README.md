af33f3567062bbee443b98a4e3c38fdee9cdecd1
Beraldo Leal <beraldo@beraldoleal.com>
2014-08-25 19:42:17 -0300

    added scripts dir

---
+++ README.md
@@ -0,0 +1,15 @@
+README
+======
+
+
+For now is a sqlite simple, just run:
+
+ export PYTHONPATH=dir:$PYTHONPATH
+ python manage.py syncdb
+ python scripts/import.py
+ python utils/deputados_fotos.py
+
+
+Run:
+
+ python manage.py runserver
---
+++ scripts/import.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from api.camara.orgaos import *
+from api.camara.deputados import *
+
+# Orgaos Webservice
+orgaos = OrgaosCamara()
+
+orgaos.importar_tipos_orgaos()
+orgaos.importar_orgaos()
+orgaos.importar_cargos()
+
+# Deputados Webservice
+deputados = DeputadosCamara()
+
+deputados.importar_partidos()
+deputados.importar_deputados()
+#deputados.importar_detalhes_deputados()
