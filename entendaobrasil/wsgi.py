22a453400f9ae1237ca68827fd57310b88672a21
Beraldo Leal <beraldo@beraldoleal.com>
2014-08-26 13:18:07 -0300

    added apache file and fixed wsgi

--- entendaobrasil/wsgi.py
+++ entendaobrasil/wsgi.py
@@ -8,7 +8,13 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/var/www/entendaobrasil'
+sys.path.insert(0, path)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "entendaobrasil.settings")
+os.environ.setdefault("PYTHON_EGG_CACHE", "/tmp/")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
